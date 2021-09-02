2432a1196eccc630a1cd005962b86bff7d2519e6
BodNS <bondarenokn@gmail.com>
2021-09-02 20:19:50 +0300

    020921

--- client/src/pages/HowItWorks/HowItWorks.js
+++ client/src/pages/HowItWorks/HowItWorks.js
@@ -71,13 +71,13 @@ return (
             <div className={styles.wrap_blue_name_2}>
                 <span class={styles.blue_name}>Our Services</span>
             </div>
-            <div>
+            
                 <h2>3 Ways To Use Squadhelp</h2>
-            </div>
+            
                 <p>Squadhelp offers 3 ways to get you a perfect name for your business.</p>
             </div>
-            <div className={styles.card_container}>
-                <div className={styles.card}>
+            <div className={styles.card_container_main}>
+                <div className={styles.card_container_2}>
                     <div className={styles.card_pict}>
                         <img src={`${CONSTANTS.STATIC_IMAGES_PATH}HIW/icon-7.svg`} alt="Card Image"/>
                     </div>
@@ -89,11 +89,11 @@ return (
                             Work with hundreds of creative experts to get custom name suggestions for your business or brand. All names are auto-checked for URL availability.
                     </p>
                     </div>
-                    <div className={styles.btn}>
+                    <div className={styles.btn_container_2}>
                         <a href="https://www.squadhelp.com/start-contest">Launch a Contest</a>
                     </div>
                 </div>
-                <div className={styles.card}>
+                <div className={styles.card_container_2}>
                 <div className={styles.card_pict}>
                         <img src={`${CONSTANTS.STATIC_IMAGES_PATH}HIW/icon-40.svg`} alt="Card Image"/>
                     </div>
@@ -105,11 +105,11 @@ return (
                         Our branding team has curated thousands of pre-made names that you can purchase instantly. All names include a matching URL and a complimentary Logo Design
                         </p>
                     </div>
-                    <div className={styles.btn}>
+                    <div className={styles.btn_container_2}>
                         <a href="https://www.squadhelp.com/premium-domains-for-sale/">Explore Names For Sale</a>
                     </div>
                 </div>
-                <div className={styles.card}>
+                <div className={styles.card_container_2}>
                 <div className={styles.card_pict}>
                         <img src={`${CONSTANTS.STATIC_IMAGES_PATH}HIW/icon-24.svg`} alt="Card Image"/>
                     </div>
@@ -121,13 +121,13 @@ return (
                             Work with hundreds of creative experts to get custom name suggestions for your business or brand. All names are auto-checked for URL availability.
                         </p>
                     </div>
-                    <div className={styles.btn}>
+                    <div className={styles.btn_container_2}>
                         <a href="https://www.squadhelp.com/managed-contests">Learn More</a>
                     </div>
                 </div>
 `            </div>
         </div>
-        <div className={styles.linehr}></div>
+        <hr className={styles.linehr} />
         <div className={styles.container_3}>
             <div className={styles.title_container_3}>
                 <div className={styles.card_pict}>
@@ -135,7 +135,7 @@ return (
                 </div>
                 <h2>How Do Naming Contests Work?</h2>
             </div>
-            <div className={styles.pict_and_spisok_container_3}>
+            <div className={styles.content_container_3}>
                 <div className={styles.pict_container_3}>
                     <img src={`${CONSTANTS.STATIC_IMAGES_PATH}HIW/support-man.svg`} alt="Card Image"/>
                 </div>
@@ -169,7 +169,7 @@ return (
             </div>
             </div>
         </div>
-        <hr /> 
+        <hr className={styles.linehr} />
         <div className={styles.container_4}>
             <div className={styles.stickyBlock}>
                 <ul>
